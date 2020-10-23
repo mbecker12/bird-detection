3fe0e4e67f9847fa59a646a6e862b085b0ddd1cb
Marvin Becker <marvin.becker2@inovex.de>
2020-10-23 12:52:03 +0200

    added script to find erroneous bboxes

---
+++ find_bad_bboxes.py
@@ -0,0 +1,25 @@
+from glob import glob
+
+for filename in glob("./data/*.txt"):
+    with open(filename, "r") as fp:
+        for line in fp.readlines():
+            try:
+                _, x, y, width, height = line.split(" ")
+            except:
+                print(f"{line=}")
+            x = float(x)
+            y = float(y)
+            width = float(width)
+            height = float(height)
+            x_min = x - width * 0.5
+            x_max = x + width * 0.5
+            y_min = y - height * 0.5
+            y_max = y + height * 0.5
+
+            try:
+                assert x_min > 0, filename
+                assert y_min > 0, filename
+                assert x_max < 1, filename
+                assert y_max < 1, filename
+            except:
+                print(f"{filename=}: {x_min=}, {x_max=}, {y_min=}, {y_max=}")
